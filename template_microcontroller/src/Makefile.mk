ad5ceb02fe6394d4c6c3bd7530335599334a4a36
Bart Bilos <squantmuts@gmail.com>
2019-03-13 22:35:27 +0100

    enabled LTO

--- template_microcontroller/src/Makefile.mk
+++ template_microcontroller/src/Makefile.mk
@@ -13,7 +13,7 @@ ALIBDIR +=
 RLIBDIR +=
 DLIBDIR +=
 DEFINES =
-COMPILE_C_FLAGS = -std=gnu11 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections
-COMPILE_CXX_FLAGS = -std=c++17 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions
+COMPILE_C_FLAGS = -std=gnu11 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -flto
+COMPILE_CXX_FLAGS = -std=c++17 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions -flto
 COMPILE_ASM_FLAGS = -c -x assembler-with-cpp
-LINK_FLAGS =
+LINK_FLAGS = -Wl,-flto
