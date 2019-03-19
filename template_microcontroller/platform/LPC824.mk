953aae47ad63977deae626f267a8fe8a3c8e8c0b
Bart Bilos <squantmuts@gmail.com>
2019-03-19 22:27:06 +0100

    added toolchain prefix per microcontroller

--- template_microcontroller/platform/LPC824.mk
+++ template_microcontroller/platform/LPC824.mk
@@ -1,7 +1,8 @@
-C_SOURCES += 
+TOOLCHAIN_PREFIX = arm-none-eabi-
+C_SOURCES +=
 CXX_SOURCES += platform/$(MCU).cpp
-S_SOURCES += 
-DEFINES += 
+S_SOURCES +=
+DEFINES +=
 COMPILE_C_FLAGS += -mcpu=cortex-m0 -mthumb
 COMPILE_CXX_FLAGS += -mcpu=cortex-m0 -mthumb
 COMPILE_ASM_FLAGS +=
