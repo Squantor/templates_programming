9b264a17519c8d20ccc40babc38e5013af086bc4
Bart Bilos <squantmuts@gmail.com>
2019-03-31 18:07:33 +0200

    added lpcopen libc and libembeddedc

--- template_lpc824/src/dummy_board.mk
+++ template_lpc824/src/dummy_board.mk
@@ -5,20 +5,33 @@ C_SOURCES +=
 CXX_SOURCES += src/$(BOARD).cpp
 S_SOURCES +=
 DEFINES += -D$(BOARD)
-INCLUDES += -Iinc
-ALIBS +=
+INCLUDES += -I../lpc_chip_82x/inc -I../squantorLibC/inc -I../squantorLibEmbeddedC/inc
+ALIBS += -llpc_chip_82x -lsqlibc -lsqlibembeddedc
 RLIBS +=
 DLIBS +=
 ALIBDIR +=
-RLIBDIR +=
-DLIBDIR +=
+RLIBDIR += -L"../lpc_chip_82x/bin/release" \
+-L"../squantorLibC/bin/CortexM0/release" \
+-L"../squantorLibEmbeddedC/bin/CortexM0/release" 
+DLIBDIR += -L"../lpc_chip_82x/bin/debug" \
+-L"../squantorLibC/bin/CortexM0/debug" \
+-L"../squantorLibEmbeddedC/bin/CortexM0/debug"
 
 #custom build rules
 pre-clean:
+	$(MAKE) -C ../lpc_chip_82x clean
+	$(MAKE) -C ../squantorLibC clean
+	$(MAKE) -C ../squantorLibEmbeddedC clean
 
 pre-release:
+	$(MAKE) -C ../lpc_chip_82x release
+	$(MAKE) -C ../squantorLibC release PLATFORM=CortexM0
+	$(MAKE) -C ../squantorLibEmbeddedC release PLATFORM=CortexM0
 
 pre-debug:
+	$(MAKE) -C ../lpc_chip_82x debug
+	$(MAKE) -C ../squantorLibC debug PLATFORM=CortexM0
+	$(MAKE) -C ../squantorLibEmbeddedC debug PLATFORM=CortexM0
 
 #project hardware specific commands
 gdbusbdebug: debug
