9c9dc9a6783e5f5e8f281e129e28dccccd4754ce
Bart Bilos <squantmuts@gmail.com>
2019-03-31 17:55:29 +0200

    update makefile features and some flaws

--- template_microcontroller/src/Makefile.mk
+++ template_microcontroller/src/Makefile.mk
@@ -1,10 +1,10 @@
-# update per change V0005
+# update per change V0006
 
 # project name
 BIN_NAME = template_microcontroller
 BOARD = dummy_board
 
-#include board specific settings
+#include board specific settings/rules
 include src/$(BOARD).mk
 
 # project settings
@@ -12,36 +12,14 @@ C_SOURCES +=
 CXX_SOURCES += src/startup.cpp src/main.cpp
 S_SOURCES +=
 INCLUDES += -Iinc
+DEFINES +=
 ALIBS +=
 RLIBS +=
 DLIBS +=
 ALIBDIR +=
 RLIBDIR +=
 DLIBDIR +=
-DEFINES +=
 COMPILE_C_FLAGS = -std=gnu11 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections
 COMPILE_CXX_FLAGS = -std=c++17 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions
 COMPILE_ASM_FLAGS = -c -x assembler-with-cpp
 LINK_FLAGS +=  -nostdlib -Wl,--gc-sections -Wl,-print-memory-usage
-
-#custom build rules
-pre-clean:
-
-pre-release:
-
-pre-debug:
-
-#project hardware specific commands
-gdbusbdebug: debug
-	$(TOOLCHAIN_PREFIX)$(GDB) -x ./gdb_scripts/bmpUSBdebug.txt
-
-gdbusbrelease: release
-	$(TOOLCHAIN_PREFIX)$(GDB) -x ./gdb_scripts/bmpUSBrelease.txt
-
-tpwrdisable:
-	$(TOOLCHAIN_PREFIX)$(GDB) -x ./gdb_scripts/bmpusb_tpwr_disable.txt
-
-tpwrenable:
-	$(TOOLCHAIN_PREFIX)$(GDB) -x ./gdb_scripts/bmpusb_tpwr_enable.txt
-
-.PHONY: gdbftdidebug gdbftdirelease gdbusbdebug gdbusbrelease tpwrdisable tpwrenable
