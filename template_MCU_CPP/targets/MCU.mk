ad4602fc28ce6e9efd81622bd1d4138f02c9a040
Squantor <squantmuts@gmail.com>
2020-04-26 09:13:19 +0200

    changed debug optimization level

--- template_MCU_CPP/targets/MCU.mk
+++ template_MCU_CPP/targets/MCU.mk
@@ -38,7 +38,7 @@ CFLAGS += -std=gnu11 -Wall -Wextra -Wno-main -fno-common -c -ffunction-sections 
 CFLAGS_debug += -O0 -g3
 CFLAGS_release += -Os -g
 CXXFLAGS += -std=c++17 -Wall -Wextra -Wno-main -fno-common -c -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions
-CXXFLAGS_debug += -O0 -g3
+CXXFLAGS_debug += -Og -g3
 CXXFLAGS_release += -Os -g
 ASMFLAGS += -c -x assembler-with-cpp
 LDFLAGS +=  -nostdlib -Wl,--gc-sections -Wl,-print-memory-usage
