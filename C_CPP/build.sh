4df668d2c156705770b350c5f7bf058bd2a6cf9b
Squantor <squantmuts@gmail.com>
2020-06-25 22:34:22 +0200

    added LPC824 template

--- C_CPP/build.sh
+++ C_CPP/build.sh
@@ -2,4 +2,6 @@
 #!/bin/bash
 make -j18 PROJECT=PC CONFIG=release
 make -j18 PROJECT=PC CONFIG=debug
+make -j18 PROJECT=LPC824 CONFIG=release
+make -j18 PROJECT=LPC824 CONFIG=debug
 make -j18 PROJECT=PC clean
