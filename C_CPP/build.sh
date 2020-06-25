2db1b583dd32b08d0256a55ef394d76d9c889068
Squantor <squantmuts@gmail.com>
2020-06-25 22:40:40 +0200

    added LPC812 template, gdb scripts and updated build test script

--- C_CPP/build.sh
+++ C_CPP/build.sh
@@ -4,4 +4,6 @@ make -j18 PROJECT=PC CONFIG=release
 make -j18 PROJECT=PC CONFIG=debug
 make -j18 PROJECT=LPC824 CONFIG=release
 make -j18 PROJECT=LPC824 CONFIG=debug
+make -j18 PROJECT=LPC812 CONFIG=release
+make -j18 PROJECT=LPC812 CONFIG=debug
 make -j18 PROJECT=PC clean
