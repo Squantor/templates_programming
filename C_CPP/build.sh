387f2dbced81b6753dd35099ec5076a9130d8288
Squantor <squantmuts@gmail.com>
2020-06-25 22:30:37 +0200

    rename of template project, it is in the templates directory, why double it up?

--- C_CPP/build.sh
+++ C_CPP/build.sh
@@ -1,5 +1,5 @@
 # build all the templates and clean afterwards
 #!/bin/bash
-make -j18 PROJECT=template_PC CONFIG=release
-make -j18 PROJECT=template_PC CONFIG=debug
-make -j18 PROJECT=template_PC clean
+make -j18 PROJECT=PC CONFIG=release
+make -j18 PROJECT=PC CONFIG=debug
+make -j18 PROJECT=PC clean
