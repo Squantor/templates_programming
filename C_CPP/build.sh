286fea2ffb57179b1bf63905d4845ff81a58230d
Squantor <squantmuts@gmail.com>
2020-07-22 23:17:45 +0200

    microcontroller templates also have a documentation generation step

--- C_CPP/build.sh
+++ C_CPP/build.sh
@@ -2,10 +2,13 @@
 #!/bin/bash
 make -j18 PROJECT=PC CONFIG=release
 make -j18 PROJECT=PC CONFIG=debug
+make -j18 PROJECT=PC doc
 make -j18 PROJECT=LPC824 CONFIG=release
 make -j18 PROJECT=LPC824 CONFIG=debug
+make -j18 PROJECT=LPC824 doc
 make -j18 PROJECT=LPC812 CONFIG=release
 make -j18 PROJECT=LPC812 CONFIG=debug
+make -j18 PROJECT=LPC812 doc
 make -j18 PROJECT=CortexM0 CONFIG=release
 make -j18 PROJECT=CortexM0 CONFIG=debug
 make -j18 PROJECT=CortexM0plus CONFIG=release
